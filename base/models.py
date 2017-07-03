c785996de9797f2699beb093ab01a6340c897c10
SimonQ <simon.mq@gmail.com>
2017-07-04 01:01:33 +0800

    增加model Profile 增加对Model Profile的测试 完成migration

--- base/models.py
+++ base/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    avatar_hash = models.CharField(max_length=32, null=True)
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{}-profile'.format(self.user.username)
+
+    def gravatar(self, size=100, default='identicon', rating='g'):
+        url = 'http://www.gravatar.com/avatar'
+        if self.avatar_hash is None:
+            email = self.user.email or 'anonymous@gmail.com'
+            import hashlib
+            self.avatar_hash = hashlib.md5(email.encode('utf-8')).hexdigest()
+            self.save()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+            url=url,
+            hash=self.avatar_hash,
+            size=size,
+            default=default,
+            rating=rating
+        )
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    else:
+        instance.profile.save()
--- base/tests.py
+++ base/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from base.models import Profile
 
 # Create your tests here.
+
+
+class ProfileModelTestCase(TestCase):
+
+    def test_profile_created_when_user_created(self):
+        from django.contrib.auth.models import User
+        user = User.objects.create_user(
+            'test_user',
+            'test_user@test.com',
+            'testpassword',
+        )
+        profile = Profile.objects.get(user_id=user.id)
+        self.assertIsNotNone(profile)
