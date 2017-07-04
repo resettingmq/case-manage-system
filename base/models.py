6da1cce070c44b0e467a95de13a4dc2d724791f4
SimonQ <simon.mq@gmail.com>
2017-07-05 00:02:15 +0800

    增加FakerMixin，实现通用的生成测试数据的办法

--- base/models.py
+++ base/models.py
@@ -52,7 +52,66 @@ class CommonFieldMixin(models.Model):
         abstract = True
 
 
-class Continent(CommonFieldMixin):
+class DescriptionFieldMixin(models.Model):
+    desc = models.TextField(null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class FakerMixin:
+    faker_fields = None
+    data_path = None
+
+    @classmethod
+    def populate(cls, count=100, locale='en_US'):
+        if cls.data_path:
+            import json
+            items = json.load(open(cls.data_path))
+            for item in items:
+                try:
+                    cls.objects.create(**item)
+                except IntegrityError:
+                    pass
+            return
+        if not cls.faker_fields:
+            raise ValueError('You must specify faker_fiedls or data_path.')
+        for fake_type in cls.faker_fields.values():
+            if isinstance(fake_type, str):
+                continue
+            if isinstance(fake_type, type) and issubclass(fake_type, models.Model):
+                continue
+            raise ValueError('fake type must be a str or a Model class inherited from models.Model')
+        try:
+            import faker
+        except ImportError:
+            print('Faker is not installed')
+        else:
+            fake = faker.Factory.create(locale)
+            fake_objs = []
+            for i in range(count):
+                data = {}
+                for field_name, fake_type in cls.faker_fields.items():
+                    is_model_class = not isinstance(fake_type, str)
+                    if is_model_class or '.' in fake_type:
+                        from random import randint
+                        if is_model_class:
+                            related_model = fake_type
+                        else:
+                            from django.utils.module_loading import import_string
+                            try:
+                                related_model = import_string(fake_type)
+                            except ImportError:
+                                raise
+                        related_obj_count = related_model.objects.count()
+                        data[field_name] = related_model.objects.all()[randint(0, related_obj_count-1)]
+                    else:
+                        data[field_name] = getattr(fake, fake_type)()
+                fake_objs.append(cls(**data))
+            cls.objects.bulk_create(fake_objs)
+
+
+class Continent(FakerMixin, CommonFieldMixin):
     id = models.CharField(max_length=5, primary_key=True)
     name_chs = models.CharField(max_length=20, db_index=True)
     name_en = models.CharField(max_length=20, db_index=True)
@@ -62,20 +121,10 @@ class Continent(CommonFieldMixin):
     def __str__(self):
         return '{c.id}-{c.name_chs}'.format(c=self)
 
-    @classmethod
-    def populate(cls):
-        import json
-        continents = json.load(open(cls.data_path))
-        for c in continents:
-            try:
-                cls.objects.create(**c)
-            except IntegrityError:
-                pass
 
-
-class Country(CommonFieldMixin):
+class Country(FakerMixin, CommonFieldMixin):
     id = models.CharField(max_length=2, primary_key=True)
-    name_chs = models.CharField(max_length=50, null=True, blank=True)
+    name_chs = models.CharField('中文名', max_length=50, null=True, blank=True)
     name_en_short = models.CharField(max_length=50)
     name_en_long = models.CharField(max_length=100, null=True, blank=True)
     calling_code = models.CharField(max_length=50)
@@ -94,18 +143,8 @@ class Country(CommonFieldMixin):
     def __str__(self):
         return '{c.id}-{c.name_en_short}-{c.name_chs}'.format(c=self)
 
-    @classmethod
-    def populate(cls):
-        import json
-        countries = json.load(open(cls.data_path))
-        for c in countries:
-            try:
-                cls.objects.create(**c)
-            except IntegrityError:
-                pass
 
-
-class Currency(CommonFieldMixin):
+class Currency(FakerMixin, CommonFieldMixin):
     id = models.CharField(max_length=5, primary_key=True)
     name_chs = models.CharField(max_length=50)
     name_en = models.CharField(max_length=50)
@@ -116,12 +155,37 @@ class Currency(CommonFieldMixin):
     class Meta:
         verbose_name_plural = 'currencies'
 
-    @classmethod
-    def populate(cls):
-        import json
-        currencies = json.load(open(cls.data_path))
-        for c in currencies:
-            try:
-                cls.objects.create(**c)
-            except IntegrityError:
-                pass
+    def __str__(self):
+        return '{c.id}-{c.name_chs}-{c.name_en}'.format(c=self)
+
+
+class Client(FakerMixin, CommonFieldMixin, DescriptionFieldMixin):
+    name = models.CharField(max_length=100)
+    is_agent = models.BooleanField(default=False)
+    tel = models.CharField(max_length=30, null=True, blank=True)
+    mobile = models.CharField(max_length=30, null=True, blank=True)
+    fax = models.CharField(max_length=30, null=True, blank=True)
+    state = models.CharField(max_length=100, null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+    address = models.CharField(max_length=200, null=True, blank=True)
+    postal_code = models.CharField(max_length=20, null=True, blank=True)
+
+    currency = models.ForeignKey(Currency, null=True, blank=True)
+    country = models.ForeignKey(Country, null=True, blank=True)
+
+    faker_fields = {
+        'name': 'company',
+        'is_agent': 'pybool',
+        'tel': 'phone_number',
+        'mobile': 'phone_number',
+        'fax': 'phone_number',
+        'state': 'state',
+        'city': 'city',
+        'address': 'street_address',
+        'postal_code': 'postcode',
+        'currency': Currency,
+        'country': 'base.models.Country',
+    }
+
+    def __str__(self):
+        return '{c.name}'.format(c=self)
