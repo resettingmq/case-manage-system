83df55bee0fcf46550cbff80093919d5268a7505
SimonQ <simon.mq@gmail.com>
2017-07-06 10:36:32 +0800

    实现了简单的ClientListView

--- base/views.py
+++ base/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+
+from . import models
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'base/index.html')
+
+
+class ClientListView(generic.ListView):
+    model = models.Client
+    template_name = 'base/client_list.html'
