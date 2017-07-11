4e23d7c1852a125b13a3691164bb3a6fea35690f
SimonQ <simon.mq@live.com>
2017-07-11 15:39:56 +0800

    为site_config.py添加简单的注释

--- cms/site_config.py
+++ cms/site_config.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 
+
+# 配置infobox的信息
 INFO_BOXES = {
     'client': {
         'model': 'base:client'
     }
 }
 
+
+# 用于配置各View的信息
+# 需要在View中指定view_name属性，来获取这个dict中相应的值
 VIEWS = {
     'index': {
         'INFO_BOXES': ['client']
