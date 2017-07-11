6cf02aec3f8104a3b3d6a6bb902401899b941fa3
SimonQ <simon.mq@live.com>
2017-07-11 16:16:46 +0800

    fix cms_widget render_infobox 使其能够同时处理字符串参数以及context参数
    修改base/index.html
    使其根据设置的infobox_dict输出infobox

--- utils/templatetags/cms_widget.py
+++ utils/templatetags/cms_widget.py
@@ -17,14 +17,18 @@ class InfoBoxNode(template.Node):
 
     def __init__(self, entity_name):
         self.entity_name = entity_name
-        template_name = '{}{}{}'.format(self.file_prefix, entity_name, self.file_suffix)
-        self.template_name = os.path.join(self.template_dir, template_name)
 
     def render(self, context):
-        t = context.template.engine.get_template(self.template_name)
+        try:
+            entity_name = self.entity_name.resolve(context)
+        except AttributeError:
+            entity_name = self.entity_name
+        template_name = '{}{}{}'.format(self.file_prefix, entity_name, self.file_suffix)
+        template_name = os.path.join(self.template_dir, template_name)
+        t = context.template.engine.get_template(template_name)
+
         new_context = template.Context()
-        new_context['infobox'] = context['infobox_dict'][self.entity_name]
-        print(new_context)
+        new_context['infobox'] = context['infobox_dict'][entity_name]
         return t.render(new_context)
 
 
@@ -39,9 +43,10 @@ def render_infobox(parser, token):
         raise template.TemplateSyntaxError(
             '{} tag requires exactly one argument: entity name'.format(token.contents.split()[0])
         )
-    if entity_name[0] in ('"', "'"):
-        entity_name = entity_name[1:]
-    if entity_name[-1] in ('"', "'"):
-        entity_name = entity_name[:-1]
+    print(entity_name)
+    if entity_name[0] == entity_name[-1] and entity_name[0] in ('"', "'"):
+        entity_name = entity_name[1:-1]
+    else:
+        entity_name = template.Variable(entity_name)
 
     return InfoBoxNode(entity_name)
