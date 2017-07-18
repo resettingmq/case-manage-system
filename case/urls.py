4dfadeba15b8114b6969ce772640922f301119c9
SimonQ <simon.mq@live.com>
2017-07-18 15:08:38 +0800

    增加case list页面和相关url conf

--- case/urls.py
+++ case/urls.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import url
+from . import views
+
+app_name = 'case'
 
 urlpatterns = [
-    # url(r'^$',),
+    url(r'^$', views.CaseListView.as_view(), name='case_list'),
 ]
--- case/views.py
+++ case/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.views import generic
 
 from utils.utils import ModelDataTable, DataTablesColumn
+from utils.views import DataTablesListView
 
 from . import models
 
@@ -12,3 +13,9 @@ class CaseDataTable(ModelDataTable):
         model = models.Case
         fields = ['name', 'client__name', 'archive_no']
         detail_url_format = '/case/{}'
+
+
+class CaseListView(DataTablesListView):
+    dt_config = CaseDataTable
+    model = models.Case
+    template_name = 'case/case_list.html'
