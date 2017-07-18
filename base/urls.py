261e0f08a1907ed25d7f590d89c28b72698b7ccf
SimonQ <simon.mq@live.com>
2017-07-18 15:07:59 +0800

    更改UrlConf配置，去掉base namespace 增加client, case namespace

--- base/urls.py
+++ base/urls.py
@@ -3,18 +3,18 @@
 from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
 from . import views
-
-app_name = 'base'
+from case import views as case_views
 
 client_urlpatterns = [
-    url(r'^$', views.ClientListView.as_view(), name='client_list'),
-    url(r'^(?P<client_id>\d+)/$', views.ClientRelatedEntityView.as_view(), name='client_detail'),
-    url(r'^create/$', views.ClientCreateView.as_view(), name='client_create'),
+    url(r'^$', views.ClientListView.as_view(), name='list'),
+    url(r'^(?P<client_id>\d+)/$', views.ClientRelatedEntityView.as_view(), name='detail'),
+    url(r'^create/$', views.ClientCreateView.as_view(), name='create'),
 ]
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
-    url(r'^client/', include(client_urlpatterns)),
+    url(r'^client/', include(client_urlpatterns, namespace='client')),
+    url(r'^case/', include('case.urls')),
 ]
