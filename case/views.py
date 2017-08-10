2aeec49ea2fbb5bed11cca3d2eec5ffc3cba850b
SimonQ <simon.mq@live.com>
2017-08-10 16:04:55 +0800

    添加case的disable以及disable validate逻辑

--- case/views.py
+++ case/views.py
@@ -42,6 +42,24 @@ class CaseDisableView(DisablementView):
     model = models.Case
     pk_url_kwarg = 'case_id'
 
+    def validate(self):
+        if any(subcase.enabled for subcase in self.object.subcase_set.all()):
+            raise ValidationError(
+                '不能删除该案件：该客户具有关联的分案',
+                code='invalid'
+            )
+
+    def disable(self):
+        # 在disable case之前，
+        # 需要先disable相关联的application和contract
+        if self.object.application:
+            self.object.application.enabled = False
+            self.object.application.save()
+        if self.object.contract:
+            self.object.contract.enabled = False
+            self.object.contract.save()
+        super().disable()
+
 
 class SubCaseListView(DataTablesListView):
     dt_config = datatables.SubCaseDataTable
